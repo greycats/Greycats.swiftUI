4db0e583a0b239cd114a99907e1cb795f1ab4b8a
Rex Sheng <shengning@gmail.com>
2022-09-27 18:52:37 +0800

    fix Toast View

--- Sources/Component/Toast.swift
+++ Sources/Component/Toast.swift
@@ -7,26 +7,27 @@
 
 import SwiftUI
 
-struct ToastHolder: Presentable, AutoDismiss {
-    var position: PresentingPosition = .bottom
-    var isPresented: Binding<Bool>
-    var duration: DispatchTimeInterval = .seconds(2)
-    var title: String?
-    var subtitle: String?
+struct Toast: View {
+    @Binding var title: String
+    @Binding var subtitle: String
+    @Binding var isPresented: Bool
 
-    @ViewBuilder func body(context: PresentableContext) -> some View {
+    var body: some View {
         GeometryReader { proxy in
             VStack(alignment: .center) {
                 Spacer()
                 HStack(alignment: .center) {
                     VStack(spacing: 8) {
-                        if let title = title {
+                        if !title.isEmpty {
                             Text(LocalizedStringKey(title))
                                 .font(Font.body.bold())
                                 .multilineTextAlignment(.center)
                                 .foregroundColor(.white)
+                        } else {
+                            Text("no title")
+                                .foregroundColor(.red)
                         }
-                        if let subtitle = subtitle {
+                        if !subtitle.isEmpty {
                             Text(LocalizedStringKey(subtitle))
                                 .font(Font.footnote)
                                 .opacity(0.7)
@@ -41,36 +42,63 @@ struct ToastHolder: Presentable, AutoDismiss {
                     .shadow(radius: 15)
                     .cornerRadius(10)
                     .onTapGesture {
-                        context.isPresented = false
+                        isPresented = false
                     }
                     .padding(.bottom, 40)
                 }
             }
-            .animation(.spring(), value: context.isPresented)
+            .animation(.spring(), value: isPresented)
             .frame(width: proxy.size.width)
         }
     }
 }
 
+struct ToastHolder: Presentable, AutoDismiss {
+    var position: PresentingPosition = .bottom
+    var isPresented: Binding<Bool>
+    var duration: DispatchTimeInterval = .seconds(2)
+    @Binding var title: String
+    @Binding var subtitle: String
+
+    @ViewBuilder func body(context: PresentableContext) -> some View {
+        Toast(title: $title, subtitle: $subtitle, isPresented: isPresented)
+    }
+}
+
 extension View {
-    public func toast(isPresented: Binding<Bool>, duration: DispatchTimeInterval = .seconds(2), title: String?, subtitle: String?) -> some View {
+    public func toast(isPresented: Binding<Bool>, duration: DispatchTimeInterval = .seconds(2), title: Binding<String>, subtitle: Binding<String> = .constant("")) -> some View {
         callout(ToastHolder(isPresented: isPresented, duration: duration, title: title, subtitle: subtitle))
+    }
+
+    public func toast(duration: DispatchTimeInterval = .seconds(2), title: Binding<String>, subtitle: Binding<String> = .constant("")) -> some View {
+        toast(isPresented: Binding<Bool>(get: {
+            !title.wrappedValue.isEmpty }, set: { if !$0 { title.wrappedValue = "" } }), duration: duration, title: title, subtitle: subtitle)
     }
 }
 
 struct Toast_Previews: PreviewProvider {
     struct ToastDemo: View {
         @State var toastIsPresented = false
+        @State var inputText: String = ""
+        @State var toastMessage: String = ""
+        @State var title = "Hello"
+        @State var subtitle = "World"
 
         var body: some View {
             VStack {
-                Spacer()
-                Button("Toast") {
+                Button("Toast \(toastMessage)") {
                     toastIsPresented.toggle()
                 }
                 .padding()
+                TextField("Echo", text: $inputText)
+                    .onSubmit {
+                        toastMessage = inputText
+                    }
+                    .padding()
             }
-            .toast(isPresented: $toastIsPresented, title: "Hello", subtitle: "World")
+            .toast(isPresented: $toastIsPresented, title: $title, subtitle: $subtitle)
+            .toast(title: $toastMessage)
+
         }
     }
     static var previews: some View {
