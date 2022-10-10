1a808c12514e39e402bfc7bce9ec7fbeb5c6a5e1
Rex Sheng <shengning@gmail.com>
2022-10-10 22:14:13 +0800

    add .hideTransition to TabView

--- Sources/Component/TabView.swift
+++ Sources/Component/TabView.swift
@@ -62,6 +62,10 @@ private struct TabVerticalPadding: EnvironmentKey {
     static let defaultValue: CGFloat = 20
 }
 
+private struct TabHideTransition: EnvironmentKey {
+    static let defaultValue: AnyTransition = .move(edge: .bottom).combined(with: .opacity)
+}
+
 extension EnvironmentValues {
     public var tabBackgroundColor: Color {
         get { self[TabBackgroundColorKey.self] }
@@ -82,6 +86,11 @@ extension EnvironmentValues {
         get { self[TabAnimationKey.self] }
         set { self[TabAnimationKey.self] = newValue }
     }
+
+    public var hideTransition: AnyTransition {
+        get { self[TabHideTransition.self] }
+        set { self[TabHideTransition.self] = newValue }
+    }
 }
 
 public struct TabView<Tab: IconTab, Content, FlyOut>: View where Content: View, FlyOut: View {
@@ -96,6 +105,7 @@ public struct TabView<Tab: IconTab, Content, FlyOut>: View where Content: View, 
     @Environment(\.tabVerticalPadding) var verticalPadding
     @Environment(\.shadowOptions) var shadowOptions
     @Environment(\.animation) var animation
+    @Environment(\.hideTransition) var hideTransition
 
     public init(selection: Binding<Tab>, tabs: Binding<[Tab]>, tabBarHidden: Binding<Bool>, @ViewBuilder content: @escaping (Tab) -> Content, @ViewBuilder flyOuts: @escaping ([Tab: CGPoint]) -> FlyOut) {
         _selection = selection
@@ -127,7 +137,7 @@ public struct TabView<Tab: IconTab, Content, FlyOut>: View where Content: View, 
             GeometryReader { geometry in
                 VStack {
                     Spacer()
-                    VStack {
+                    if !tabBarHidden {
                         HStack {
                             Spacer()
                             ForEach(tabs, id: \.self) { tab in
@@ -136,15 +146,15 @@ public struct TabView<Tab: IconTab, Content, FlyOut>: View where Content: View, 
                             }
                         }
                         .padding(.vertical, verticalPadding)
+                        .transition(hideTransition)
+                        .background(
+                            tabBackgroundColor
+                                .edgesIgnoringSafeArea(.bottom)
+                                .shadow(color: shadowOptions.color, radius: shadowOptions.radius, x: shadowOptions.x, y: shadowOptions.y)
+                        )
                     }
-                    .animation(.easeIn(duration: 0.25), value: tabBarHidden)
-                    .padding(.bottom, tabBarHidden ? -geometry.safeAreaInsets.bottom : 0)
-                    .background(
-                        tabBackgroundColor
-                            .edgesIgnoringSafeArea(.bottom)
-                            .shadow(color: shadowOptions.color, radius: shadowOptions.radius, x: shadowOptions.x, y: shadowOptions.y)
-                    )
                 }
+                .animation(.easeOut(duration: 0.25), value: tabBarHidden)
                 flyOuts(preferences)
             }
             .onPreferenceChange(TabBarPreferences<Tab>.self) { sizes in
@@ -158,21 +168,23 @@ extension TabView where FlyOut == EmptyView {
     public init(selection: Binding<Tab>, tabs: Binding<[Tab]>, tabBarHidden: Binding<Bool>, @ViewBuilder content: @escaping (Tab) -> Content) {
         self.init(selection: selection, tabs: tabs, tabBarHidden: tabBarHidden, content: content, flyOuts: { _ in EmptyView() })
     }
+
+    public init(selection: Binding<Tab>, tabs: [Tab], tabBarHidden: Binding<Bool>, @ViewBuilder content: @escaping (Tab) -> Content) {
+        self.init(selection: selection, tabs: .constant(tabs), tabBarHidden: tabBarHidden, content: content, flyOuts: { _ in EmptyView() })
+    }
 }
 
-
-extension TabView where Tab: CaseIterable, Tab.AllCases: RandomAccessCollection {
+extension TabView where Tab: CaseIterable {
     public init(selection: Binding<Tab>, tabBarHidden: Binding<Bool>, @ViewBuilder content: @escaping (Tab) -> Content, @ViewBuilder flyOuts: @escaping ([Tab: CGPoint]) -> FlyOut) {
         self.init(selection: selection, tabs: .constant(Tab.allCases as! [Tab]), tabBarHidden: tabBarHidden, content: content, flyOuts: flyOuts)
     }
 }
 
-extension TabView where Tab: CaseIterable, Tab.AllCases: RandomAccessCollection, FlyOut == EmptyView {
+extension TabView where Tab: CaseIterable, FlyOut == EmptyView {
     public init(selection: Binding<Tab>, tabBarHidden: Binding<Bool>, @ViewBuilder content: @escaping (Tab) -> Content) {
-        self.init(selection: selection, tabs: .constant(Tab.allCases as! [Tab]), tabBarHidden: tabBarHidden, content: content, flyOuts: { _ in EmptyView() })
+        self.init(selection: selection, tabs: Tab.allCases as! [Tab], tabBarHidden: tabBarHidden, content: content)
     }
 }
-
 
 struct TabView_Previews: PreviewProvider {
 
@@ -193,7 +205,6 @@ struct TabView_Previews: PreviewProvider {
             .background(
                 RoundedRectangle(cornerRadius: 22)
                     .fill(selected ? Color(red: 0.404, green: 0.420, blue: 0.529) : .clear)
-
             )
         }
 
@@ -243,6 +254,9 @@ struct TabView_Previews: PreviewProvider {
                 VStack(spacing: 50) {
                     Button("\(hidden ? "show" : "hide") tabs") {
                         hidden.toggle()
+                        if hidden {
+                            flyOutVisible = false
+                        }
                     }
                     Button("\(flyOutVisible ? "hide" : "show") flyOut") {
                         flyOutVisible.toggle()
